c2c89a39250056b7d5c86a65aa7f1ddd942f9f5e
devetechnologies <devetechnologies@gmail.com>
2024-07-12 06:45:37 -0400

    completed changes in pages

--- customers/urls.py
+++ customers/urls.py
@@ -4,7 +4,7 @@ from . import views #this point mean that is in the same directory
 urlpatterns = [
     path('home',views.home, name="home"),
     path('aboutUs',views.aboutUs, name="aboutUs"),
-    path('services',views.aboutUs, name="services"),
-    path('contact',views.aboutUs, name="contact"),
+    path('services',views.services, name="services"),
+    path('contact',views.contact, name="contact"),
    
 ]
